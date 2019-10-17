66a69aae5d413cd4026cc28f536530bb3571306d
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-17 13:57:08 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,13 +1,13 @@
 def hello_t(array)
+  if block_given?
+   i = 0 
   
-  i = 0 
+    while i < array.length 
+      yield array[i]
+     i  = i + 1 
+   end 
   
-  while i < array.length 
-    yield array[i]
-    i  = i + 1 
-  end 
-  
-  array 
+   array 
   
 end
 
