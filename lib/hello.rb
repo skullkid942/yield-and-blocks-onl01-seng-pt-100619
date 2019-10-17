fd7ae4ea364518269da61e67be1547ae588388c9
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-17 13:56:44 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,13 @@
 def hello_t(array)
   i = 0 
-  
+  if block_given?
   while i < array.length 
     yield array[i]
     i  = i + 1 
   end 
   
   array 
+  
 end
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
